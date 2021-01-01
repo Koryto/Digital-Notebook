bf2f86c03cb70ed8c92ef0d2ab42439fe3dcfa4f
Koryto <mykoryto93@gmail.com>
2021-01-01 16:27:19 +0200

    structure built

--- app/app.py
+++ app/app.py
@@ -1,11 +1,23 @@
 from flask import Flask
-from config import Config
-from flask_mongoengine import MongoEngine
+
+from ..config import Config
+from flask import render_template
+
+
 
 app = Flask(__name__)
 app.config.from_object(Config)
 
-db = MongoEngine()
-db.init_app(app)
+#db = MongoEngine()
+#db.init_app(app)
 
-from app import routes
+
+
+@app.route("/") # Root directory
+@app.route("/index")
+def index():
+    return render_template("index.html")
+
+#@app.route("/login")
+#def login():
+#    return render_template("login.html")
